cc1ad67e65129ee1bf18d280eeea61803646013c
danny <your.email@example.com>
2025-07-23 09:26:46 +0800

    edit html

---
+++ testPrj/app.ts
@@ -0,0 +1,4 @@
+const Student object  {
+    ID: number,
+
+}
---
+++ testPrj/script.js
@@ -0,0 +1,6 @@
+const textinput1 = document.querySelector("#Textinput1");
+const textinput2 = document.querySelector("#Textinput2");
+const button=document.querySelector("button")
+button.addEventListener("click",()=>{
+    console.log('Clicked')
+})
